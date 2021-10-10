34cd71131a56fd6d624e5d46679d1ff2a3c8bb07
procedurecall <procedurecall@gmail.com>
2021-10-09 21:52:09 -0400

    Add build and run script

---
+++ build_and_run.sh
@@ -0,0 +1,2 @@
+bundle exec jekyll serve
+echo "\nEnter _site directory and push contents to gh-pages branch to push to production website (delete and pull that branch as _site directory first if on new machine, before re-building)"
